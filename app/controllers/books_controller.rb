7d9769e6d4794125ab0b3902d39d78e064b097c8
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-05-11 20:07:33 +0000

    [Update] update時のバリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,17 +25,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
-  
+
   private
   # ストロングパラメータ
   def book_params
