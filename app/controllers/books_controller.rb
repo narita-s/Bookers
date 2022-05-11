4816430c2e158fc0be570d4c72433bed7538a0bc
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-05-11 13:11:07 +0000

    [Update] 削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,13 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
   private
   # ストロングパラメータ
   def book_params
